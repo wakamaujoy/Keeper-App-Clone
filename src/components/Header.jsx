3840c7ecedecea40b72939dba87c49c3bef1d446
wakamaujoy <wakamaujoy@gmail.com>
2024-01-28 12:21:07 +0300

    Appclone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import LightbulbIcon from '@mui/icons-material/Lightbulb';
 function Header(){
     return(
         <header>
-        <h1><LightbulbIcon/>Keeper App</h1></header>
+        <h1><LightbulbIcon/>Keep</h1></header>
     )
 }
 export default Header;
